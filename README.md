a938708dd980fe1405f7f83b8c1f898e16681298
PedroIan <pedroianmoraes@gmail.com>
2023-06-09 10:34:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flattenTeste
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/flatten-node-7hygrz)
---
+++ src/index.js
@@ -0,0 +1,27 @@
+/**
+ * Desafio: escrever uma função que receba um array com valores ou sub-arrays, e retorne todos os valores abertos em um único array
+ * Todos os valores serão compostos por numeros inteiros.
+ *
+ * IMPORTANTE:
+ * Não devem ser utilizadas as funções flat() ou flatMap() dos arrays.
+ * A ideia é avaliar a lógica utilizada, e não a utilização dos métodos prontos.
+ *
+ * Voce pode testar o seu codigo rodando o comando `npm run test` no terminal
+ * e tambem pode alterar o arquivo `index.test.js` se desejar.
+ * Apos enviado, seu codigo sera validado com outros cenarios de teste tambem.
+ *
+ * @example `flatten([1, [2, 3], 4, [5, 6]])` retorna `[1, 2, 3, 4, 5, 6]`
+ * @param numbers array de inteiros ou sub-arrays
+ * @returns novo array, somente com os valores
+ */
+function flatten(array) {
+  // implemente o codigo aqui
+  //
+  return array.map((element) => {
+    console.log(element);
+    if (Array.isArray(element)) return flatten(element);
+    else return element;
+  });
+}
+
+module.exports = flatten;

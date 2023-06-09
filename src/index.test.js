867f142451af65d56eaceb2d98d9bf90e147d992
PedroIan <pedroianmoraes@gmail.com>
2023-06-09 10:50:00 -0300

    1 file modified

--- src/index.test.js
+++ src/index.test.js
@@ -16,4 +16,8 @@ test('Uso do flatten()', () => {
   expect(flatten([1, [2, 3], 4, [5, 6]])).toEqual([1, 2, 3, 4, 5, 6]);
   expect(flatten([[1, 2, 3]])).toEqual([1, 2, 3]);
   expect(flatten([1, 2, 3])).toEqual([1, 2, 3]);
+
+  // Testes adicionais pessoais
+  expect(flatten([1, [[2], 3], 4, [5, [6]]])).toEqual([1, 2, 3, 4, 5, 6]);
+  expect(flatten([1, [[[2], 3], 4], [5, [6]]])).toEqual([1, 2, 3, 4, 5, 6]);
 });
